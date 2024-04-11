c9f8e34594d279e63d94719d56e78d6eb7f52b1f
kanishkadz <kanishkaadz@gmail.com>
2024-04-11 18:37:17 +0530

    ok

---
+++ razorpay_gateway/lib/razorpay_payment.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:razorpay_flutter/razorpay_flutter.dart';
+
+class RazorPayPage extends StatefulWidget {
+  const RazorPayPage({super.key});
+
+  @override
+  State<RazorPayPage> createState() => _RazorPayPageState();
+}
+
+class _RazorPayPageState extends State<RazorPayPage> {
+  late Razorpay _razorpay;
+  TextEditingController amtController = new TextEditingController();
+
+  void openCheckout(amount) async {
+    amount = amount * 100;
+    var options = {
+      'key': 'rzp_test_1DP5mm0lF5G5ag',
+      'amount': amount,
+      'name': 'Kanishka Anand',
+      'prefill': {'contact': '99xxxxxxxx', 'email': 'kanishkaadz@gmail.com'},
+      'external': {
+        'wallets': ['paytm']
+      }
+    };
+    try {
+      _razorpay.open(options);
+    } catch (e) {
+      debugPrint('Error : e');
+    }
+  }
+
+  void handlePaymentSuccess(PaymentSuccessResponse response) {
+    Fluttertoast.showToast(
+        msg: "Payment Successful " + response.paymentId!,
+        toastLength: Toast.LENGTH_SHORT);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
