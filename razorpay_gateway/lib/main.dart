d800dcb6f1867786bd16be2425686216215d7742
kanishkadz <kanishkaadz@gmail.com>
2024-04-11 21:38:52 +0530

    ok

--- razorpay_gateway/lib/main.dart
+++ razorpay_gateway/lib/main.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:razorpay_gateway/razorpay_payment.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
